59b7293539cb970f0cfd51c2519179b3c502e6c8
gjkt2001 <2910223554@qq.com>
2023-12-06 09:19:40 +0800

    若依的ID生成工具类-雪花算法

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -3,13 +3,28 @@ package com.ruoyi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class RuoYiApplication
 {
     public static void main(String[] args)
--- ruoyi-admin/src/test/java/com/ruoyi/web/PssTest.java
+++ ruoyi-admin/src/test/java/com/ruoyi/web/PssTest.java
@@ -1,5 +1,7 @@
 package com.ruoyi.web;
 
+import com.ruoyi.common.utils.uuid.IdUtils;
+import com.ruoyi.common.utils.uuid.UUID;
 import com.ruoyi.pill.domain.PillDrug;
 import com.ruoyi.pill.domain.PillFactory;
 import com.ruoyi.pill.service.IPillDrugService;
@@ -9,22 +11,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author xyz66 Email:2910223554@qq.com
  * @date 2023/11/7 13:38
  */
 
-@SpringBootTest
+//@SpringBootTest
 public class PssTest {
     @Autowired
     private IPillFactoryService pillFactoryService;
 
     @Autowired
-    private  IPillDrugService iPillDrugService;
+    private IPillDrugService iPillDrugService;
 
     @Test
-    public void testSelect(){
+    public void testSelect() {
         PillFactory pillFactory = new PillFactory();
         pillFactory.setFactoryName("云南");
 //        pillFactory.setFactoryId(101L);
@@ -32,7 +35,7 @@ public class PssTest {
     }
 
     @Test
-    public void testSelectDrug(){
+    public void testSelectDrug() {
         PillDrug pillDrug = new PillDrug();
         PillFactory pillFactory = new PillFactory();
         pillFactory.setFactoryName("哈药");
@@ -40,5 +43,55 @@ public class PssTest {
         List<PillDrug> pillDrugs = iPillDrugService.selectPillDrugList(pillDrug);
         System.out.println(pillDrugs);
     }
+
+    @Test
+    public void cs1() {
+        // 测试雪花ID
+//        System.out.println(UUID.randomUUID());
+//        System.out.println(UUID.fastUUID());
+        System.out.println(UUID.nameUUIDFromBytes("123".getBytes()));
+    }
+
+    @Test
+    public void cs2() {
+        // 测试ID生成工具类
+        long l = System.currentTimeMillis();// 起始毫秒数
+        System.out.println(IdUtils.randomUUID());
+        long end = System.currentTimeMillis();
+        System.out.println(end - l);
+    }
+
+    @Test
+    public void cs3() {
+        // 去除"-"符号
+        long l = System.currentTimeMillis();// 起始毫秒数
+        System.out.println(IdUtils.simpleUUID());
+        long end = System.currentTimeMillis();
+        System.out.println(end - l);
+    }
+
+    @Test
+    public void cs4() {
+        long l = System.currentTimeMillis();// 起始毫秒数
+        // 性能更好的单独线程生成ID，去除"-"符号
+        System.out.println(IdUtils.fastSimpleUUID());
+        long end = System.currentTimeMillis();
+        System.out.println(end - l);
+    }  
+    @Test
+    public void cs5() {
+        long l = System.currentTimeMillis();// 起始毫秒数
+        // 性能更好的单独线程生成ID
+        System.out.println(IdUtils.fastUUID());
+        long end = System.currentTimeMillis();
+        System.out.println(end - l);
+    }
+    @Test
+    public void cs6() {
+        ThreadLocalRandom current = ThreadLocalRandom.current();// 获得随机数生成器，线程安全且性能高效
+        int i = current.nextInt(100);
+        System.out.println(i);
+    }
+    
 
 }
